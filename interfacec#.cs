9f5c991c6edfa15d7efc7284103edb3fb5641dc3
meet675 <142784290+meet675@users.noreply.github.com>
2024-05-01 17:34:19 +0530

    Add files via upload

---
+++ interfacec#.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+interface IShape
+{
+    double CalculateArea();
+}
+
+
+class Rectangle : IShape { 
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public double CalculateArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        Rectangle rectangle = new Rectangle(5, 10);
+
+     
+        double area = rectangle.CalculateArea();
+
+        Console.WriteLine("Area of the rectangle: " + area);
+    }
+}
